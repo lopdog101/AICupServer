352f67a71d47b6d94ca7781385e632e267457d5f
azhel12 <azhel12reg@gmail.com>
2014-12-08 18:16:30 +0300

    Change: replace #endif to end

--- dev/Cell.hpp
+++ dev/Cell.hpp
@@ -1,4 +1,21 @@
 #ifndef CELL_HPP
 #define CELL_HPP
 
+namespace AICup {
+   namespace Dev {
+
+        struct Cell
+        {
+            char _horizontal;
+            char _mainDiagonal;
+            char _vertical;
+            char _secondDiagonal;
+
+            char _gamer;
+        };
+
+    }
+}
+
 #endif // CELL_HPP
+
